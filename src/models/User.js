39228f327df861347578c292c6df20ada3f34fc5
Diego Bittencourt <diego6d@hotmail.com>
2022-12-14 14:32:07 -0300

    Update longitude/latitude type from decimal to REAL

--- src/models/User.js
+++ src/models/User.js
@@ -27,8 +27,10 @@ class User extends Model {
             company: DataTypes.STRING,
             position: DataTypes.STRING,
             about: DataTypes.STRING,
-            lastLongitude: DataTypes.DECIMAL(10, 6),
-            lastLatitude: DataTypes.DECIMAL(10, 6),
+            // lastLongitude: DataTypes.DECIMAL(10, 6),
+            // lastLatitude: DataTypes.DECIMAL(10, 6),
+            lastLongitude: DataTypes.REAL,
+            lastLatitude: DataTypes.REAL,
             lastTimeSuperLikeWasUsed: DataTypes.DATE,
         }, {
             sequelize,//conexão
